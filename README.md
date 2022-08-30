bb8b80b51a192941809dc53618612394d055da4c
calwhitehouse <whitehouse2845@gmail.com>
2022-08-30 22:40:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# MERN Learning Project
+
+This is a simple MERN stack project that allows an admin to add/edit/delete information relating to animals.
+
+## Installation
+
+Use package manager npm to install the project.
+
+```bash
+npm install
+```
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react"
+import {createRoot} from "react-dom/client"
+import Axios from "axios"
+import CreateNewForm from "./components/CreateNewForm"
+import AnimalCard from "./components/AnimalCard"
+
+function App() {
+    const [animals, setAnimals] = useState([])
+
+    useEffect(() => {
+        async function go() {
+            const response = await Axios.get("/api/animals")
+            setAnimals(response.data)
+        }
+        go()
+    }, [])
+    return (
+        <div className="container">
+            <p><a href="/">&laquo; Back to the home page</a></p>
+            <CreateNewForm setAnimals={setAnimals} />
+            <div className="animal-grid">
+            {animals.map(function(animal) {
+                return <AnimalCard key={animal._id} name={animal.name} type={animal.type} photo={animal.photo} id={animal._id} setAnimals={setAnimals} />
+            })}
+            </div>
+        </div>
+    )
+}
+
+const root = createRoot(document.querySelector("#app"))
+root.render(<App />)
